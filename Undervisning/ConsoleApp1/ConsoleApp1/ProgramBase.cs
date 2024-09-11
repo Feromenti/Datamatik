0ccd24e94a53af7c1421d32c44c2c2dc8fe1def2
Feromenti <apps.feromenti@gmail.com>
2024-09-11 14:22:05 +0200

    11.09 opgave 3

---
+++ Undervisning/ConsoleApp1/ConsoleApp1/ProgramBase.cs
@@ -0,0 +1,7 @@
+﻿internal class ProgramBase
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Undervisning/Pr03_CSharpData/Pr03_CSharpData/Program.cs
@@ -0,0 +1,86 @@
+﻿
+class Program 
+{
+   
+    static void Main(string[] args)
+    {
+        string userInput = "";
+        
+        
+        while (userInput.ToLower() != "done" )
+        {   
+            // giver mulighed hat skipe opgaven ( virker ikke ;/ )
+            Console.WriteLine("Write 'done' to skip task\n" + "press any button to continue");
+            userInput = Console.ReadLine();
+
+            // spørger efter breden
+            Console.WriteLine("Input width");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            // spørger efter længten
+            Console.WriteLine("Input length");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            //giver arealet ud til consolen
+            Console.WriteLine("your answer is:\n" + (width * length));
+
+            // spørger om du vil beregne et andet areal eller gå til næste opgave
+            Console.WriteLine("\nWrite 'done' if you´re done");
+            Console.WriteLine("else press enter\n");
+            userInput = Console.ReadLine();
+        
+        }
+
+        userInput = "";
+
+
+        while (userInput.ToLower() != "done")
+        {
+
+            //Laver et String array der gemmer det første tupel 
+            Console.WriteLine("Enter the first tuple in the format: x1, y1:");
+            string[] startPointInput = Console.ReadLine().Split(',');
+
+            // parser string til double
+            double x1 = Convert.ToDouble(startPointInput[0].Trim());
+            double y1 = Convert.ToDouble(startPointInput[1].Trim());
+
+            //Laver et String array der gemmer det andet tupel 
+            Console.WriteLine("Enter the second tuple in the format: x2, y2:");
+            string[] endPointInput = Console.ReadLine().Split(',');
+
+            // parser string til double
+            double x2 = Convert.ToDouble(endPointInput[0].Trim());
+            double y2 = Convert.ToDouble(endPointInput[1].Trim());
+             
+            // beregner hældingen og giver den til consolen
+            double slope = (y2 - y1) / (x2 - x1);
+            Console.WriteLine($"The slope of the line segment is: {slope}");
+
+            // giver mulighed til afslute med opgaven eller gentage regningen
+            Console.WriteLine("\nWrite 'done' if you´re done");
+            Console.WriteLine("else press enter\n");
+            userInput = Console.ReadLine();
+
+        }
+
+        userInput = "";
+
+        while (userInput.ToLower() != "done")
+        {
+            //indlæser stringen
+            Console.WriteLine("\nWrite a Random sentence");
+            string randomString = Console.ReadLine();
+
+            //udgiver string længde
+            Console.WriteLine("\nyou string has a lenght of:\n");
+            Console.WriteLine(randomString.Length);
+
+            // giver mulighed til afslute med opgaven eller gentage regningen
+            Console.WriteLine("\nWrite 'done' if you´re done");
+            Console.WriteLine("else press enter\n");
+            userInput = Console.ReadLine();
+        }
+
+    }
+}
