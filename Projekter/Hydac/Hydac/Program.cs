772975d32cf5029cebe5c1f1627aad18c9c242ba
Feromenti <apps.feromenti@gmail.com>
2024-09-18 17:53:30 +0200

    first commit

---
+++ Projekter/Hydac/Hydac/Program.cs
@@ -0,0 +1,54 @@
+﻿class Guestbook
+{
+
+    static void Main(string[] args)
+    {        
+        bool running = true;
+        bool first_run = true;
+        Util utils = new Util();
+
+        while (running)
+        {
+            if (first_run)
+            {
+                utils.getWelcomesText();
+                first_run = false;
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("User ID:");
+            string user_id = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine();
+
+            if (utils.userExist(user_id, password) != true)
+            {
+                Console.WriteLine(
+                    """
+                    Login failed!
+
+                    press enter to retry.
+
+                    """);
+                continue;
+            }
+
+            Console.Clear();
+
+            Console.WriteLine(
+                $"""
+                Hello {user_id}
+
+                choose an option:
+
+                1. Find visitors
+                2. End program
+                
+                """);            
+        }          
+    }
+}
---
+++ Projekter/Hydac/Hydac/Utils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+public class Util
+{
+    string[,] login_data = 
+        {
+        { "JohnJ", "123456" },
+        { "user2", "123456"}
+        };
+
+    string[,] guest_book =
+        {
+        {"Julien","Microsoft","Medium","18.09.24","13.30-15.30" },
+        {"","","","","" },
+        {"","","","","" }
+        };
+    public bool userConfirmation(char y)
+	{
+		if (Console.ReadLine() == "y")
+		{ 
+			return true;
+		}
+		return false;	
+	}
+    public bool userExist(string a, string b)
+    {        
+        for (int i = 0; i < login_data.GetLength(0); i++)
+        {            
+            if (a == login_data[i, 0] && b == login_data[i, 1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void getWelcomesText()
+    {
+        Console.WriteLine(
+            """                                                                            
+             _____       _            _____             _   _           _   
+            |  |  |_ _ _| |___ ___   |   __|_ _ ___ ___| |_| |_ ___ ___| |_ 
+            |     | | | . | .'|  _|  |  |  | | | -_|_ -|  _| . | . | . | '_|
+            |__|__|_  |___|__,|___|  |_____|___|___|___|_| |___|___|___|_,_|
+                  |___|                                                     
+
+
+
+
+
+
+
+                     _____                    _____     _                           
+                    |  _  |___ ___ ___ ___   |   __|___| |_ ___ ___                 
+                    |   __|  _| -_|_ -|_ -|  |   __|   |  _| -_|  _|                
+                    |__|  |_| |___|___|___|  |_____|_|_|_| |___|_|                  
+
+
+                     _____                    _   _                                 
+                    |_   _|___    ___ ___ ___| |_|_|___ _ _ ___                     
+                      | | | . |  |  _| . |   |  _| |   | | | -_|                    
+                      |_| |___|  |___|___|_|_|_| |_|_|_|___|___|                    
+                                                                                                                                                                                                                                                                               
+            """);    
+    }
+}
